56a1686753a8767dce7e0ea69af59d8c6435d161
Zahid Hasan <zh05698@gmail.com>
2026-07-23 18:10:27 +0600

    Fix: Clean folder structure and ignore node_modules

--- node_modules/@react-native-community/cli/build/index.d.ts
+++
@@ -1,6 +0,0 @@
-import loadConfig, { loadConfigAsync } from '@react-native-community/cli-config';
-export { createDevServerMiddleware } from '@react-native-community/cli-server-api';
-declare function run(platformName?: string): Promise<void>;
-declare const bin: string;
-export { run, bin, loadConfig, loadConfigAsync };
-//# sourceMappingURL=index.d.ts.map
