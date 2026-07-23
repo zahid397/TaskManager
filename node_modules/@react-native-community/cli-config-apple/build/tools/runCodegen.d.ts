98998ea44b82c1dd4a4bfca65a188f09220ea983
Zahid Hasan <zh05698@gmail.com>
2026-07-23 18:26:15 +0600

    Refactor: Clean folder structure and remove untracked node_modules

--- node_modules/@react-native-community/cli-config-apple/build/tools/runCodegen.d.ts
+++
@@ -1,9 +0,0 @@
-interface CodegenOptions {
-    root: string;
-    platform: string;
-    reactNativePath: string;
-    iosFolderPath: string;
-}
-declare function runCodegen(options: CodegenOptions): Promise<void>;
-export default runCodegen;
-//# sourceMappingURL=runCodegen.d.ts.map
